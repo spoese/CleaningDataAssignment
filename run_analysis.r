781b7b874fd8db077469991ff078635ee02b4858
spoese <scott.poese@gmail.com>
2018-06-04 07:53:13 -0400

    Steps 1-4
    Finished the first four steps of the assignment.

--- run_analysis.r
+++ run_analysis.r
@@ -1,6 +1,10 @@
+##The following steps download the file, unzip the file, and read in the
+##various data sets
+
 ##fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 ##download.file(fileUrl,destfile = "./CleanData.zip"); unzip("./CleanData.zip") 
 ##file.rename("UCI HAR Dataset","CleanData")
+##features <- read.table("./CleanData/features.txt") #561 x 2
 ##activity_labels <- read.table("./CleanData/activity_labels.txt") #6 x 2
 ##subject_test <- read.table("./CleanData/test/subject_test.txt") #2947 x 1
 ##X_test <- read.table("./CleanData/test/X_test.txt") #2947 x 561
@@ -8,3 +12,21 @@
 ##subject_train <- read.table("./CleanData/train/subject_train.txt") #7352 x 1
 ##X_train <- read.table("./CleanData/train/X_train.txt") #7352 x 561
 ##y_train <- read.table("./CleanData/train/y_train.txt") #7352 x 1
+
+##Now we are looking to create one universal dataset.
+test <- cbind(subject_test,y_test,X_test)
+train <- cbind(subject_train,y_train,X_train)
+data <- rbind(test,train)
+
+##Trying to parse out only the mean and std of the measurements
+mean_std_vars <- features[
+        grepl("mean\\(\\)",features[,2]) | grepl("std\\(\\)",features[,2]),]
+mean_std_data <- data[,c(1,2,mean_std_vars[,1]+2)]
+
+##Using descriptive names to name the activities in the data set
+mean_std_data[,2] <- sapply(mean_std_data[,2],function(x) activity_labels[x,2])
+
+#Labeling data set with descriptive variable names
+mean_std_vars[,2] <- gsub("\\(\\)","",mean_std_vars[,2])
+cnames <- c("Subject","Activity",mean_std_vars[,2])
+colnames(mean_std_data) <- cnames
