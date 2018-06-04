68469ed27efc8a72bde55757518d903413769c6b
spoese <scott.poese@gmail.com>
2018-06-04 09:10:34 -0400

    Code Final
    Code final, comments may be updated.

--- run_analysis.r
+++ run_analysis.r
@@ -1,17 +1,17 @@
 ##The following steps download the file, unzip the file, and read in the
 ##various data sets
 
-##fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-##download.file(fileUrl,destfile = "./CleanData.zip"); unzip("./CleanData.zip") 
-##file.rename("UCI HAR Dataset","CleanData")
-##features <- read.table("./CleanData/features.txt") #561 x 2
-##activity_labels <- read.table("./CleanData/activity_labels.txt") #6 x 2
-##subject_test <- read.table("./CleanData/test/subject_test.txt") #2947 x 1
-##X_test <- read.table("./CleanData/test/X_test.txt") #2947 x 561
-##y_test <- read.table("./CleanData/test/y_test.txt") #2947 x 1
-##subject_train <- read.table("./CleanData/train/subject_train.txt") #7352 x 1
-##X_train <- read.table("./CleanData/train/X_train.txt") #7352 x 561
-##y_train <- read.table("./CleanData/train/y_train.txt") #7352 x 1
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile = "./CleanData.zip"); unzip("./CleanData.zip") 
+file.rename("UCI HAR Dataset","CleanData")
+features <- read.table("./CleanData/features.txt") 
+activity_labels <- read.table("./CleanData/activity_labels.txt") 
+subject_test <- read.table("./CleanData/test/subject_test.txt") 
+X_test <- read.table("./CleanData/test/X_test.txt") 
+y_test <- read.table("./CleanData/test/y_test.txt")
+subject_train <- read.table("./CleanData/train/subject_train.txt")
+X_train <- read.table("./CleanData/train/X_train.txt")
+y_train <- read.table("./CleanData/train/y_train.txt")
 
 ##Now we are looking to create one universal dataset.
 test <- cbind(subject_test,y_test,X_test)
@@ -33,8 +33,5 @@ colnames(mean_std_data) <- cnames
 
 #Filtering down to only the averages
 msd_melt <- melt(mean_std_data,id = c("Subject","Activity"))
-msd_melt <- msd_melt %>%
-        mutate(new.factor = interaction(Subject,Activity,variable)) %>%
-        mutate(averages = ave(value,new.factor))
-msd_melt <- unique(msd_melt[,c(1,2,3,5,6)])
-msd_melt <- arrange(msd_melt[c(1:3,5)],Subject,Activity,variable)
+msd_melt <- ddply(msd_melt,.(Subject,Activity,variable),summarize,
+                  Averages = mean(value))
